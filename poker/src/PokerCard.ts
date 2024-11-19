d7fdcee2124d1388d3ab4e5914ec7f1f5df8a223
Igor Balos <ibalosh@gmail.com>
2024-11-19 14:52:26 +0100

    cleanup

--- poker/src/PokerCard.ts
+++ poker/src/PokerCard.ts
@@ -15,13 +15,13 @@ export class PokerCard implements Card{
   constructor(identifier: PokerCardIdentifier) {
     this.identifier = identifier;
 
-    this.value = this.values[identifier.charAt(0)];
-    this.suit = this.suits[identifier.charAt(1)];
+    this.value = this.valuesAsNumbers[identifier.charAt(0)];
+    this.suit = this.suitsAsNumbers[identifier.charAt(1)];
   }
 
-  private suits:StringNumberPair = {"s":0, "h": 1, "d": 2, "c": 3}
+  private suitsAsNumbers:StringNumberPair = {"s":0, "h": 1, "d": 2, "c": 3}
 
-  private values:StringNumberPair = {
+  private valuesAsNumbers:StringNumberPair = {
     "0": 0, "1": 1, "2": 2, "3": 3, "4": 4,
     "5": 5, "6": 6, "7": 7, "8": 8, "9": 9,
     "T": 10, "J": 11, "Q": 12, "K": 13, "A": 14
